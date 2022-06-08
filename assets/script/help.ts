8bcb33cf66aa213180341d80a00e1a23666791de
Nguyễn Ngọc Châu Bùi <nnchau1293@outlook.com>
2022-06-08 15:48:03 +0700

    feat: Help animation
    Hiệu ứng này nọ cho nút bấm "trợ giúp"

--- assets/script/help.ts
+++ assets/script/help.ts
@@ -5,13 +5,19 @@
 // Learn life-cycle callbacks:
 //  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const { ccclass, property } = cc._decorator;
+const { ccclass } = cc._decorator
 
 @ccclass
 export default class Help extends cc.Component {
-	onHelpClick()
-	{
-		this.node.getChildByName("popup").active = !this.node.getChildByName("popup").active
+	popup: cc.Node = null
+	isActive: boolean = false
+	onHelpClick() {
+		if (this.isActive) {
+			cc.tween(this.popup).to(0.2, { opacity: 0 }).start()
+		} else {
+			cc.tween(this.popup).to(0.2, { opacity: 255 }).start()
+		}
+		this.isActive = !this.isActive
 	}
 
 	// LIFE-CYCLE CALLBACKS:
@@ -19,7 +25,7 @@ export default class Help extends cc.Component {
 	// onLoad () {}
 
 	start() {
-
+		this.popup = this.node.getChildByName("popup")
 	}
 
 	// update (dt) {}
